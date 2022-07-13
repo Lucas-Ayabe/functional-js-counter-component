47df4407bda63d50fb5515f3643dad473b70d5df
Lucas-Ayabe <lucas.ayabe@gmail.com>
2022-07-12 23:14:27 -0300

    feat: added optional support

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 import { $, append } from "./lib/dom";
 import { Counter } from "./components/counter";
 
-$("#app").apply(Counter(0).bind(append)).run();
+const appDoesntExistsHandler = () => ({
+  append: () => console.error("app doesn't exist."),
+});
+
+$("#app")
+  .map((appOrNone) => {
+    return appOrNone.matchWith({
+      none: appDoesntExistsHandler,
+      just: (app) => app,
+    });
+  })
+  .apply(Counter(0).bind(append))
+  .run();
